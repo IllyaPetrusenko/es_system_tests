3ad70b6e38962356b22dc8a75cba9c06c53d36be
tatarenko245 <tatarenko.roman@gmail.com>
2020-10-01 20:47:04 +0300

    feature(tests/bpe_create_ei/payloads.py) add random function for ENUM checking

--- tests/bpe_create_ei/payloads.py
+++ tests/bpe_create_ei/payloads.py
@@ -1,62 +1,88 @@
+import random
+
+typeOfBuyer = ('BODY_PUBLIC', 'EU_INSTITUTION', 'MINISTRY', 'NATIONAL_AGENCY', 'REGIONAL_AGENCY', 'REGIONAL_AUTHORITY',)
+mainGeneralActivity = (
+    'DEFENCE', 'ECONOMIC_AND_FINANCIAL_AFFAIRS', 'EDUCATION', 'ENVIRONMENT', 'GENERAL_PUBLIC_SERVICES', 'HEALTH',
+    'HOUSING_AND_COMMUNITY_AMENITIES', 'PUBLIC_ORDER_AND_SAFETY', 'RECREATION_CULTURE_AND_RELIGION',
+    'SOCIAL_PROTECTION',)
+mainSectoralActivity = ('AIRPORT_RELATED_ACTIVITIES', 'ELECTRICITY', 'EXPLORATION_EXTRACTION_COAL_OTHER_SOLID_FUEL',
+                        'EXPLORATION_EXTRACTION_GAS_OI',
+                        'PORT_RELATED_ACTIVITIES', 'POSTAL_SERVICES', 'PRODUCTION_TRANSPORT_DISTRIBUTION_GAS_HEAT',
+                        'RAILWAY_SERVICES', 'URBAN_RAILWAY_TRAMWAY_TROLLEYBUS_BUS_SERVICES', 'WATER',)
+
 ei_full = {
-	"tender": {
-		"title": "EI_FULL_WORKS",
-		"description": "description of finansical sourse",
-		"classification": {
-			"id": "24600000-0"
-		}
-	},
-	"planning": {
-		"budget": {
-			"period": {
-				"startDate": "2020-09-25T13:21:46Z",
-				"endDate": "2020-09-30T13:21:46Z"
-			}
-		},
-		"rationale": "planning.rationale"
-	},
-	"buyer": {
-		"name": "LLC Petrusenko",
-		"identifier": {
-			"id": "380632074071",
-			"scheme": "MD-IDNO",
-			"legalName": "LLC Petrusenko",
-			"uri": "http://petrusenko.com/fop"
-		},
-		"address": {
-			"streetAddress": "Zakrevskogo",
-			"postalCode": "02217",
-			"addressDetails": {
-				"country": {
-					"id": "MD"
-				},
-				"region": {
-					"id": "1700000"
-				},
-				"locality": {
-					"scheme": "CUATM",
-					"id": "1701000",
-					"description": "description"
-				}
-			}
-		},
-		"additionalIdentifiers": [{
-			"id": "string",
-			"scheme": "MD-IDNO",
-			"legalName": "380935103469",
-			"uri": "http://petrusenko.com/svetlana"
-		}],
-		"contactPoint": {
-			"name": "Petrusenko Svitlana",
-			"email": "svetik@gmail.com",
-			"telephone": "888999666",
-			"faxNumber": "5552233",
-			"url": "http://petrusenko.com/svetlana"
-		},
-		"details": {
-			"typeOfBuyer": "NATIONAL_AGENCY",
-			"mainGeneralActivity": "RECREATION_CULTURE_AND_RELIGION",
-			"mainSectoralActivity": "URBAN_RAILWAY_TRAMWAY_TROLLEYBUS_BUS_SERVICES"
-		}
-	}
+    "tender": {
+        "title": "EI_FULL_WORKS",
+        "description": "description of finansical sourse",
+        "mainProcurementCategory": "",
+        "classification": {
+            "id": "45100000-8",
+            "scheme": "CPV",
+            "description": "classification.description"
+        }
+    },
+    "planning": {
+        "budget": {
+            "id": "45100000-8",
+            "period": {
+                "startDate": "2020-01-01T00:00:00Z",
+                "endDate": "2020-12-31T00:00:00Z"
+            }
+        },
+        "rationale": "planning.rationale"
+    },
+    "buyer": {
+        "name": "LLC Petrusenko",
+        "identifier": {
+            "id": "380632074071",
+            "scheme": "MD-IDNO",
+            "legalName": "LLC Petrusenko",
+            "uri": "http://petrusenko.com/fop"
+        },
+        "address": {
+            "streetAddress": "Zakrevskogo",
+            "postalCode": "02217",
+            "addressDetails": {
+                "country": {
+                    "id": "MD",
+                    "scheme": "",
+                    "description": "",
+                    "uri": ""
+                },
+                "region": {
+                    "id": "1700000",
+                    "scheme": "",
+                    "description": "",
+                    "uri": ""
+                },
+                "locality": {
+                    "scheme": "CUATM",
+                    "id": "1701000",
+                    "description": "description",
+                    "uri": ""
+                }
+            }
+        },
+        "additionalIdentifiers": [
+            {
+                "id": "string",
+                "scheme": "MD-IDNO",
+                "legalName": "380935103469",
+                "uri": "http://petrusenko.com/svetlana"
+            }
+        ],
+        "contactPoint": {
+            "name": "Petrusenko Svitlana",
+            "email": "svetik@gmail.com",
+            "telephone": "888999666",
+            "faxNumber": "5552233",
+            "url": "http://petrusenko.com/svetlana"
+        },
+        "details": {
+            "typeOfBuyer": f'{random.choice(typeOfBuyer)}',
+            "mainGeneralActivity": f'{random.choice(mainGeneralActivity)}',
+            "mainSectoralActivity": f'{random.choice(mainSectoralActivity)}'
+
+        }
+    }
 }
