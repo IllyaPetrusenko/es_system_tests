2aa59584bb16552a3f7f45e17094007293330d00
tatarenko245 <tatarenko.roman@gmail.com>
2020-10-09 19:37:53 +0300

    feature(tests/bpe_update_ei/update_ei.py) add function bpe_update_ei

--- tests/bpe_update_ei/update_ei.py
+++ tests/bpe_update_ei/update_ei.py
@@ -7,12 +7,12 @@ from tests.kafka_messages import get_message_from_kafka
 from tests.bpe_create_ei.payloads import ei_full
 from tests.bpe_create_ei.create_ei import bpe_create_ei
 
-def bpe_update_ei(payload):
+def bpe_update_ei(payload, ei_create_payload):
     access_token = get_access_token_for_platform_one()
     x_operation_id = get_x_operation_id(access_token)
-    ei = copy.deepcopy(ei_full)
-    create_ei_response = bpe_create_ei(ei)
-    request_to_create_ei = requests.post(
+    create_ei_response = bpe_create_ei(ei_create_payload)
+    time.sleep(2)
+    request_to_update_ei = requests.post(
         url=host + update_ei + create_ei_response[1]['data']['outcomes']['ei'][0]['id'],
         headers={
             'Authorization': 'Bearer ' + access_token,
@@ -22,7 +22,7 @@ def bpe_update_ei(payload):
         json=payload)
     time.sleep(1)
     message_from_kafka = get_message_from_kafka(x_operation_id)
-    return request_to_create_ei, message_from_kafka, x_operation_id
+    return request_to_update_ei, message_from_kafka, x_operation_id
 
 # ei=copy.deepcopy(ei_full)
 # xc = bpe_update_ei(ei)
