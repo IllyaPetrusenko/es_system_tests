a3fe5e6385a41da1029dbdd86610f86a57ccab8c
tatarenko245 <tatarenko.roman@gmail.com>
2020-11-26 12:47:23 +0200

    feature(tests/bpe_create_ei/payloads.py): add ei_obligatory.tender.items

--- tests/bpe_create_ei/payloads.py
+++ tests/bpe_create_ei/payloads.py
@@ -1,18 +1,18 @@
 import random
 
-typeOfBuyer = ('BODY_PUBLIC', 'EU_INSTITUTION', 'MINISTRY', 'NATIONAL_AGENCY', 'REGIONAL_AGENCY', 'REGIONAL_AUTHORITY',)
+typeOfBuyer = ("BODY_PUBLIC", "EU_INSTITUTION", "MINISTRY", "NATIONAL_AGENCY", "REGIONAL_AGENCY", "REGIONAL_AUTHORITY",)
 mainGeneralActivity = (
-    'DEFENCE', 'ECONOMIC_AND_FINANCIAL_AFFAIRS', 'EDUCATION', 'ENVIRONMENT', 'GENERAL_PUBLIC_SERVICES', 'HEALTH',
-    'HOUSING_AND_COMMUNITY_AMENITIES', 'PUBLIC_ORDER_AND_SAFETY', 'RECREATION_CULTURE_AND_RELIGION',
-    'SOCIAL_PROTECTION',)
-mainSectoralActivity = ('AIRPORT_RELATED_ACTIVITIES', 'ELECTRICITY', 'EXPLORATION_EXTRACTION_COAL_OTHER_SOLID_FUEL',
-                        'EXPLORATION_EXTRACTION_GAS_OI',
-                        'PORT_RELATED_ACTIVITIES', 'POSTAL_SERVICES', 'PRODUCTION_TRANSPORT_DISTRIBUTION_GAS_HEAT',
-                        'RAILWAY_SERVICES', 'URBAN_RAILWAY_TRAMWAY_TROLLEYBUS_BUS_SERVICES', 'WATER',)
-cpv_goods = ('03100000-2', '39100000-3', '48600000-4',)
-cpv_works = ('45100000-8', '45200000-9', '45100000-8',)
-cpv_services = ('76100000-4', '76200000-5', '90900000-6',)
-locality_scheme = ('CUATM', 'other',)
+    "DEFENCE", "ECONOMIC_AND_FINANCIAL_AFFAIRS", "EDUCATION", "ENVIRONMENT", "GENERAL_PUBLIC_SERVICES", "HEALTH",
+    "HOUSING_AND_COMMUNITY_AMENITIES", "PUBLIC_ORDER_AND_SAFETY", "RECREATION_CULTURE_AND_RELIGION",
+    "SOCIAL_PROTECTION",)
+mainSectoralActivity = ("AIRPORT_RELATED_ACTIVITIES", "ELECTRICITY", "EXPLORATION_EXTRACTION_COAL_OTHER_SOLID_FUEL",
+                        "EXPLORATION_EXTRACTION_GAS_OI",
+                        "PORT_RELATED_ACTIVITIES", "POSTAL_SERVICES", "PRODUCTION_TRANSPORT_DISTRIBUTION_GAS_HEAT",
+                        "RAILWAY_SERVICES", "URBAN_RAILWAY_TRAMWAY_TROLLEYBUS_BUS_SERVICES", "WATER",)
+cpv_goods = ("03100000-2", "39100000-3", "48600000-4",)
+cpv_works = ("45100000-8", "45200000-9", "45100000-8",)
+cpv_services = ("76100000-4", "76200000-5", "90900000-6",)
+locality_scheme = ("CUATM", "other",)
 ei_full = {
     "tender": {
         "title": "EI_FULL_WORKS",
@@ -133,49 +133,88 @@ ei_full = {
 
 ei_obligatory = {
     "tender": {
-        "title": "Cardurilor de acces pentru Bibliotecii Municipale B.P. Hasdeu ",
+        "title": "EI_FULL_WORKS",
+
+        "mainProcurementCategory": "",
         "classification": {
             "id": "45100000-8"
 
-        }
+        },
+        "items": [
+            {
+                "id": "1",
+                "description": "item 1",
+                "classification": {
+                    "id": "45100000-8"
+                },
+
+                "deliveryAddress": {
+
+                    "addressDetails": {
+                        "country": {
+                            "id": "MD"
+
+                        },
+                        "region": {
+                            "id": "1700000"
+
+                        }
+
+                    }
+                },
+                "quantity": 1,
+                "unit": {
+                    "id": "10"
+
+                }
+            }
+        ]
     },
     "planning": {
         "budget": {
             "id": "45100000-8",
             "period": {
                 "startDate": "2020-01-01T00:00:00Z",
-                "endDate": "2020-12-31T12:40:00Z"
+                "endDate": "2020-12-31T00:00:00Z"
             }
         }
+
     },
     "buyer": {
-        "name": "Directia Cultura a Primariei mun.Chisinau",
+        "name": "LLC Petrusenko",
         "identifier": {
-            "id": "123654789000",
+            "id": "380632074071",
             "scheme": "MD-IDNO",
-            "legalName": "Directia Cultura a Primariei mun.Chisinau"
+            "legalName": "LLC Petrusenko"
+
         },
         "address": {
-            "streetAddress": "str.Bucuresti 68",
+            "streetAddress": "Zakrevskogo",
+
             "addressDetails": {
                 "country": {
                     "id": "MD"
+
                 },
                 "region": {
-                    "id": "0101000"
+                    "id": "1700000"
+
                 },
                 "locality": {
                     "scheme": "CUATM",
-                    "id": "0101000",
-                    "description": "mun.Chişinău"
+                    "id": "1701000",
+                    "description": "description"
+
                 }
             }
-
         },
+
         "contactPoint": {
-            "name": "Dumitru Popa",
-            "email": "directiacultшra@yahoo.com",
-            "telephone": "022242290"
+            "name": "Petrusenko Svitlana",
+            "email": "svetik@gmail.com",
+            "telephone": "888999666"
+
         }
+
     }
 }
