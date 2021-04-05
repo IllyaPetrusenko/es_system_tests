95777887905149f37abd6e11cff9f697fcd234ee
illya.petrusenko@gmail.com <0445450232a>
2021-04-05 22:46:53 +0300

    2021-04-05: 16:57  -> add allure steps in create ei (regression)

--- tests/bpe_create_ei/test_for_test.py
+++ tests/bpe_create_ei/test_for_test.py
@@ -18,7 +18,7 @@ class TestCheckTheImpossibilityToCreateEIWithoutObligatoryData(object):
         ei.create_request_ei(payload=payload, lang=language, country=country)
         actual_result = ei.get_message_from_kafka()
         actual_result = actual_result['errors']
-        expected_result = '[{"code":"400.00.00.00","description":"Data processing exception."}]'
+        expected_result = "[{'code':'400.00.00.00','description':'Data processing exception.'}]"
         allure.attach(expected_result, 'Expected result')
         allure.attach(actual_result, 'Actual result')
         assert actual_result == expected_result
