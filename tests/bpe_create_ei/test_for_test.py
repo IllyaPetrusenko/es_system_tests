be5c34a0478165eaf1f89534188b41b75db18620
illya.petrusenko@gmail.com <0445450232a>
2021-04-05 17:32:32 +0300

    2021-04-05: 16:57  -> add allure steps in create ei (regression)

--- tests/bpe_create_ei/test_for_test.py
+++ tests/bpe_create_ei/test_for_test.py
@@ -6,35 +6,85 @@ from tests.bpe_create_ei.create_ei import EI
 from tests.bpe_create_ei.payloads import payload_ei_full_data_model
 
 
-@allure.title("Check the impossibility to create EI without obligatory data")
-class Test22132(object):
+@pytestrail.case("22132")
+class CheckTheImpossibilityToCreateEIWithoutObligatoryData(object):
 
-    @allure.step("Delete tender object from the payload.")
-    @pytest.mark.smoke
-    @pytest.mark.regression
+    @allure.step('Delete tender object from request')
     @pytestrail.case("22132")
-    def test_22132_1(self, country, language):
+    @pytest.mark.regression
+    @pytest.mark.smoke
+    def test_22132_1(self, country, language, tag):
+        if tag == "regression":
+            ei = EI()
+            payload = copy.deepcopy(payload_ei_full_data_model)
+            del payload["tender"]
+            create_ei_response = ei.create_request_ei(payload=payload, lang=language, country=country)
+            message_from_kafka = ei.get_message_from_kafka()
+            assert create_ei_response.text == "ok"
+            assert create_ei_response.status_code == 202
+            assert message_from_kafka["errors"][0]["code"] == "400.00.00.00"
+            assert message_from_kafka["errors"][0]["description"] == "Data processing exception."
+        elif tag == "smoke":
+                ei = EI()
+                payload = copy.deepcopy(payload_ei_full_data_model)
+                del payload["tender"]
+                create_ei_response = ei.create_request_ei(payload=payload, lang=language, country=country)
+                message_from_kafka = ei.get_message_from_kafka()
+                assert create_ei_response.text == "ok"
+                assert create_ei_response.status_code == 202
+                assert message_from_kafka["errors"][0]["code"] == "400.00.00.00"
+                assert message_from_kafka["errors"][0]["description"] == "Data processing exception."
+
+    @allure.step('Delete tender title from request')
+    @pytestrail.case("22132")
+    @pytest.mark.regression
+    @pytest.mark.smoke
+    def test_22132_2(self, country, language):
         ei = EI()
         payload = copy.deepcopy(payload_ei_full_data_model)
-        del payload["tender"]
+        del payload["tender"]["title"]
         create_ei_response = ei.create_request_ei(payload=payload, lang=language, country=country)
         message_from_kafka = ei.get_message_from_kafka()
         assert create_ei_response.text == "ok"
         assert create_ei_response.status_code == 202
-        assert message_from_kafka["errors"][0]["code"] == "400.00.00.00"
-        assert message_from_kafka["errors"][0]["description"] == "Data processing exception."
+        assert message_from_kafka["errors"][0]["code"] == "400.10.00"
+        assert message_from_kafka["errors"][0]["description"] == "com.fasterxml.jackson.module.kotlin.Missing" \
+                                                                 "KotlinParameterException: Instantiation of " \
+                                                                 "[simple type, class com.procurement.budget." \
+                                                                 "model.dto.ei.request.EiCreate$TenderEiCreate] " \
+                                                                 "value failed for JSON property title due to " \
+                                                                 "missing (therefore NULL) value for creator " \
+                                                                 "parameter title which is a non-nullable " \
+                                                                 "type\n at [Source: UNKNOWN; line: -1, " \
+                                                                 "column: -1] (through reference chain: com." \
+                                                                 "procurement.budget.model.dto.ei.request." \
+                                                                 "EiCreate[\"tender\"]->com.procurement.budget." \
+                                                                 "model.dto.ei.request.EiCreate$TenderEiCreate" \
+                                                                 "[\"title\"])"
 
-    @allure.step("Delete tender object from the payload.")
-    @pytest.mark.smoke
-    @pytest.mark.regression
+    @allure.step('Delete tender classification from request')
     @pytestrail.case("22132")
-    def test_22132_1(self, country, language):
+    @pytest.mark.regression
+    @pytest.mark.smoke
+    def test_22132_3(self, country, language):
         ei = EI()
         payload = copy.deepcopy(payload_ei_full_data_model)
-        del payload["tender"]
+        del payload["tender"]["classification"]
         create_ei_response = ei.create_request_ei(payload=payload, lang=language, country=country)
         message_from_kafka = ei.get_message_from_kafka()
         assert create_ei_response.text == "ok"
         assert create_ei_response.status_code == 202
-        assert message_from_kafka["errors"][0]["code"] == "400.00.00.00"
-        assert message_from_kafka["errors"][0]["description"] == "Data processing exception."
+        assert message_from_kafka["errors"][0]["code"] == "400.20.00"
+        assert message_from_kafka["errors"][0]["description"] == "com.fasterxml.jackson.module.kotlin.Missing" \
+                                                                 "KotlinParameterException: Instantiation of " \
+                                                                 "[simple type, class com.procurement.mdm.model." \
+                                                                 "dto.data.ei.EIRequest$Tender] value failed for " \
+                                                                 "JSON property classification due to missing " \
+                                                                 "(therefore NULL) value for creator parameter " \
+                                                                 "classification which is a non-nullable type\n " \
+                                                                 "at [Source: UNKNOWN; line: -1, column: -1] " \
+                                                                 "(through reference chain: com.procurement.mdm." \
+                                                                 "model.dto.data.ei.EIRequest[\"tender\"]->com." \
+                                                                 "procurement.mdm.model.dto.data.ei.EIRequest$" \
+                                                                 "Tender[\"classification\"])"
+
