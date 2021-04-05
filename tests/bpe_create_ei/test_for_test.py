458041f832bb441745942ef02f9eee2636fda76f
illya.petrusenko@gmail.com <0445450232a>
2021-04-05 22:24:43 +0300

    2021-04-05: 16:57  -> add allure steps in create ei (regression)

--- tests/bpe_create_ei/test_for_test.py
+++ tests/bpe_create_ei/test_for_test.py
@@ -17,11 +17,12 @@ class TestCheckTheImpossibilityToCreateEIWithoutObligatoryData(object):
         del payload["tender"]
         create_ei_response = ei.create_request_ei(payload=payload, lang=language, country=country)
         actual_result = ei.get_message_from_kafka()
+        actual_result = str(actual_result['errors'])
         assert create_ei_response.text == "ok"
         assert create_ei_response.status_code == 202
         expected_result = '[{"code":"400.00.00.00","description":"Data processing exception."}]'
         allure.attach(str(expected_result), 'Expected result')
-        assert str(actual_result['errors']) == expected_result
+        assert actual_result == expected_result
 
     @allure.step('Delete tender title from request')
     @pytestrail.case("22132")
