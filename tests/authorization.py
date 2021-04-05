9b956b966a9de23b71f4c03accd7028347027ff3
illya.petrusenko@gmail.com <0445450232a>
2021-04-05 21:39:29 +0300

    2021-04-05: 16:57  -> add allure steps in create ei (regression)

--- tests/authorization.py
+++ tests/authorization.py
@@ -2,7 +2,6 @@ import requests, allure
 from config import host
 
 
-@allure.step('Get access token')
 def get_access_token_for_platform_one():
     access_token = requests.get(
         url=host + '/auth/signin',
@@ -13,7 +12,6 @@ def get_access_token_for_platform_one():
     return access_token
 
 
-@allure.step('Get x-operation-id')
 def get_x_operation_id(platform_token):
     x_operation_id = requests.post(
         url=host + '/operations',
