fc728c456d15563945a2fd8e401f8e95189be4d3
tatarenko245 <tatarenko.roman@gmail.com>
2021-04-06 17:35:32 +0300

    06/04/2021 17:35: -> tests/kafka_message.py: refactor for allure

--- tests/kafka_messages.py
+++ tests/kafka_messages.py
@@ -7,9 +7,8 @@ from pprint import pprint
 def get_message_from_kafka(x_operation_id):
     kafka_message = requests.get(
         url=kafka_host + '/x-operation-id/' + x_operation_id
-    )
-    allure.attach(kafka_message, "Message from Kafka")
-    kafka_message = kafka_message.json()
+    ).json()
     del kafka_message['_id']
+    allure.attach(str(kafka_message), "Message from Kafka")
     return kafka_message
 
