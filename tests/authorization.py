0a6338ee9ff664ad09a5bfa5e9dfbd35d73cfb87
illya.petrusenko@gmail.com <0445450232a>
2021-04-07 10:21:34 +0300

    2021-04-07: 09:50  -> add logging for x-operation-id in authorization.py

--- tests/authorization.py
+++ tests/authorization.py
@@ -11,24 +11,24 @@ def get_access_token_for_platform_one():
             'Authorization': platform_1
         }).json()
     allure.attach(host + '/auth/signin', 'HOST')
+    allure.attach(platform_1, 'Platform credentials for authorization')
     allure.attach(json.dumps(access_token), 'Response from auth service')
     access_token = access_token['data']['tokens']['access']
-    allure.attach(platform_1, 'Platform credentials for authorization')
     allure.attach(str(access_token), 'Access token')
     return access_token
 
 
 @allure.step('Get X-OPERATION-ID')
 def get_x_operation_id(platform_token):
-    allure.attach(platform_token, 'Platform access token')
     x_operation_id = requests.post(
         url=host + '/operations',
         headers={
             'Authorization': 'Bearer ' + platform_token
         }).json()
+    allure.attach(host + '/operations', 'HOST')
+    allure.attach(platform_token, 'Platform access token')
     allure.attach(json.dumps(x_operation_id), 'Response from auth service')
     x_operation_id = x_operation_id['data']['operationId']
-    allure.attach(host + '/operations', 'HOST')
     allure.attach(str(x_operation_id), 'X-OPERATION-ID')
     return x_operation_id
 
