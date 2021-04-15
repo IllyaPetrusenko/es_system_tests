78310466cc68fba86257f7f6b5a0ff2fed539cbd
tatarenko245 <tatarenko.roman@gmail.com>
2021-04-15 22:29:12 +0300

    15/04/2021 22:30: -> tests/essences/ei.py: refactor method 'get_message_from_kafka' of class EI => add allure -> tests/kafka_messages.py: refactor method 'get_message_from_kafka' => del allure

--- tests/kafka_messages.py
+++ tests/kafka_messages.py
@@ -3,12 +3,11 @@ import requests, json
 from config import kafka_host
 from pprint import pprint
 
-@allure.step('Receive message in feed-point')
+
 def get_message_from_kafka(x_operation_id):
     kafka_message = requests.get(
         url=kafka_host + '/x-operation-id/' + x_operation_id
     ).json()
     del kafka_message['_id']
-    allure.attach(json.dumps(kafka_message), 'Message in feed-point')
     return kafka_message
 
